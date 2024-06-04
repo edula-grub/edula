399d8557238ebcff4b68dc0f9a48566568944526
lgarin211 <lagustinus211@gmail.com>
2024-06-04 11:24:45 +0700

    Masukin perubahan sedikit

--- database/migrations/2024_06_01_083706_create_bidrequests_table.php
+++ database/migrations/2024_06_01_083706_create_bidrequests_table.php
@@ -12,21 +12,21 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('bidrequests', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->integer('siswa_id');
-            $table->integer('guru_id');
-            $table->text('nama_mapel');
-            $table->longText('deskripsi_kebutuhan');
-            $table->dateTime('jadwal');
-            $table->float('harga_bider_terpilih');
-            $table->longText('status');
-            $table->longText('review');
-            $table->longText('komentar_guru_ke_siswa');
-            $table->longText('komen_siswa_ke_guru');
-            $table->integer('rating_guru_ke_siswa');
-            $table->integer('rating_siswa_ke_guru');
-            $table->longText('taskpoinlain');
-            $table->longText('zoomlink');
+            $table->bigIncrements('id')->nullable();
+            $table->integer('siswa_id')->nullable();
+            $table->integer('guru_id')->nullable();
+            $table->text('nama_mapel')->nullable();
+            $table->longText('deskripsi_kebutuhan')->nullable();
+            $table->dateTime('jadwal')->nullable();
+            $table->float('harga_bider_terpilih')->nullable();
+            $table->longText('status')->nullable();
+            $table->longText('review')->nullable();
+            $table->longText('komentar_guru_ke_siswa')->nullable();
+            $table->longText('komen_siswa_ke_guru')->nullable();
+            $table->integer('rating_guru_ke_siswa')->nullable();
+            $table->integer('rating_siswa_ke_guru')->nullable();
+            $table->longText('taskpoinlain')->nullable();
+            $table->longText('zoomlink')->nullable();
             $table->timestamps();
         });
     }

eae7c2b9d1f98e6fecc79ca991671c51a696df35
lgarin211 <lagustinus211@gmail.com>
2024-07-22 13:42:42 +0700

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -12,58 +12,27 @@ use App\Http\Controllers\ipaymuController;
 Route::get('/', function () {
     return view('welcome');
 });
-// Route::get('/login', function () {
-//     return view('login');
-// });
-// Auth::routes();
-
 Route::get('/login', [AuthController::class, 'login']);
-// Route::get('/register', [AuthController::class, 'register']);
 Route::POST('/login', [AuthController::class, 'ValidateLogin'])->name('login');
 Route::POST('/register', [AuthController::class, 'ValidateRegister'])->name('register');
-
-
 Route::get('/CaraKerjaPelajar', function () {
     return view('caraKerjaPelajar');
 });
-
 Route::get('/CaraKerjaPengajar', function(){
     return view('caraKerjaPengajar');
 });
-
 Route::get('/landing', function () {
     return view('landing');
 });
-
-// Route::get('/riwayatpengajar',function() {
-//     return view('ktks');
-// });
-
 Route::get('/riwayatpengajar', [PengajarController::class, 'riwayatpengajar'])->name('riwayatpengajar');
-// Route::get('/riwayatpengajar/{id}', [PengajarController::class, 'riwayatpengajar'])->name('riwayatpengajar.show');
-
-// Route::get('/riwayatpengajar2',function() {
-//     return view('rr');
-// });
-
-
-
-
-// agustinus
-// Detail1
 Route::get('/detail1', function () {
     return view('detail1');
 });
-// DetailPengajar
 Route::get('/DetailPengajar', function () {
     return view('DetailPengajar');
 });
 
 
-
-
-// yohhhhh
-// UbahProfilPengajar
 Route::get('/UbahProfilPengajar', function () {
     return view('UbahProfilPengajar');
 });
@@ -87,34 +56,18 @@ Route::get('/riwayatpengajar2',function() {
     return view('rr');
 });
 
-// agustinus
-// Detail1
+
 Route::get('/detail1', function () {
     return view('detail1');
 });
 
-// Route::post('/detail1', function (Request $req) {
-//     dd($req->all());
-//     return view('detail1');
-// });
 Route::post('/form-review', [PelajarController::class, 'store'])->name('form-review');
 
-// DetailPengajar
-// Route::get('/DetailPengajar', function () {
-//     return view('DetailPengajar');
-// });
-
-// DetailPengajar.show
 Route::get('/DetailPengajar/{guru_id}',[GuruController::class, 'show'])->name('DetailPengajar.show');
 Route::post('/DetailPengajar/{guru_id}/upload',[GuruController::class, 'store'])->name('DetailPengajar.store');
 
 Route::get('/DetailPengajar',[GuruController::class, 'showall'])->name('DetailPengajar.showall');
 
-
-
-
-// yohhhhh
-// UbahProfilPengajar
 Route::get('/UbahProfilPengajar', function () {
     return view('UbahProfilPengajar');
 });
@@ -154,3 +107,43 @@ Route::get('/ReqKelasConfirm', function(){
 Route::get('/ReqKelasSchedule', function(){
     return view('Pelajar.RequestKelasSchedule');
 });
+
+// DetailPengajar
+// agustinus
+// Detail1
+// Route::get('/register', [AuthController::class, 'register']);
+// Route::get('/login', function () {
+//     return view('login');
+// });
+// Auth::routes();
+// Route::get('/riwayatpengajar',function() {
+//     return view('ktks');
+// });
+// Route::get('/riwayatpengajar/{id}', [PengajarController::class, 'riwayatpengajar'])->name('riwayatpengajar.show');
+// Route::get('/riwayatpengajar2',function() {
+//     return view('rr');
+// });
+
+
+// yohhhhh
+// UbahProfilPengajar
+
+// DetailPengajar
+// Route::get('/DetailPengajar', function () {
+//     return view('DetailPengajar');
+// });
+
+
+// Route::post('/detail1', function (Request $req) {
+//     dd($req->all());
+//     return view('detail1');
+// });
+// DetailPengajar.show
+
+// agustinus
+// Detail1
+
+
+
+// yohhhhh
+// UbahProfilPengajar

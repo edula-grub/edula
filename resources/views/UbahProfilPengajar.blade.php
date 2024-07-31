6ba620d395010c91e2e6d580b8574f8b6827496e
Yohanna Y <yohyoe18@gmail.com>
2024-07-31 13:23:43 +0700

    pushhterakhir

--- resources/views/UbahProfilPengajar.blade.php
+++ resources/views/UbahProfilPengajar.blade.php
@@ -14,29 +14,32 @@
     <main class="rectangle-parent51">
         <div class="frame-child100"></div>
         <div class="ubah-profil-parent">
-            <b class="ubah-profil8">Ubah Profil</b>
+            <b class="ubah-profil8">Ubah Profil </b>
             <div class="frame-parent82">
                 <div class="personal-data-label-row-parent">
                     <div class="personal-data-label-row">
-                        <img class="id-card-24dp-fill0-wght400-gra-icon2" loading="lazy" alt="" src="{{url('EdulaExport/public/id-card-24dp-fill0-wght400-grad0-opsz24-1.svg')}}" />
+                        <img class="id-card-24dp-fill0-wght400-gra-icon2" loading="lazy" alt="" src="{{url('EdulaExport/public/id_card_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg')}}" />
                     </div>
-                    <div class="data-pribadi2">Data Pribadi</div>
+                    <div class="data-pribadi2" onclick="render(1)">Data Pribadi</div>
                 </div>
                 <div class="frame-parent83">
                     <div class="password-24dp-fill0-wght400-gr-container">
                         <img class="password-24dp-fill0-wght400-gr-icon1" loading="lazy" alt="" src="{{url('EdulaExport/public/password-24dp-fill0-wght400-grad0-opsz24-1.svg')}}" />
                     </div>
-                    <div class="kata-sandi1">Kata Sandi</div>
+                    <div class="kata-sandi1" onclick="render(2)">Kata Sandi</div>
                 </div>
                 <div class="frame-parent84">
                     <div class="policy-24dp-fill0-wght400-grad-container">
                         <img class="policy-24dp-fill0-wght400-grad-icon1" loading="lazy" alt="" src="{{url('EdulaExport/public/policy-24dp-fill0-wght400-grad0-opsz24-1.svg')}}" />
                     </div>
-                    <a class="kebijakan1">Kebijakan</a>
+                    <a class="kebijakan1" style="text-decoration: none" onclick="render(3)">Kebijakan</a>
                 </div>
             </div>
         </div>
         <div class="frame-child101"></div>
+
+        {{-- yang bagian kanan, yang bisa diganti-ganti (form) --}}
+        <div class="poinrender" id="renderpick">
         <form class="frame-parent85" method="POST" action="{{route('updateprofil')}}">
             @csrf
             <div class="frame-wrapper59">
@@ -53,7 +56,7 @@
                 </div>
             </div>
             <div class="foto-profil-group">
-                <a class="foto-profil1">Foto Profil</a>
+                <div class="alamat-email1">Foto Profil</div>
                 <div class="frame-parent86">
                     <div class="frame-parent87">
                         <img class="frame-child103" loading="lazy" alt="" src="{{$datasiswa->profile}}" />
@@ -61,9 +64,9 @@
                     </div>
                     <div class="edit-picture-buttons">
                         <div class="edit-picture-button-container">
-                            <div class="button28">
+                            {{-- <div class="button28">
                                 <div class="ubah-foto-profil1">Ubah Foto Profil</div>
-                            </div>
+                            </div> --}}
                             <div class="button29">
                                 <div class="hapus-foto-profil1">Hapus Foto Profil</div>
                             </div>
@@ -76,10 +79,10 @@
                             <div class="fill-namadepan-item"></div>
                             <input class="nama-depan1" placeholder="Nama depan" value="{{$datasiswa->name}}" type="text" name="namadepan" />
                         </div>
-                        <div class="fill-namabelakang1">
+                        {{-- <div class="fill-namabelakang1">
                             <div class="fill-namabelakang-item"></div>
                             <input class="nama-belakang1" placeholder="Nama belakang" value="" type="text" name="namabelakang"/>
-                        </div>
+                        </div> --}}
                     </div>
                     <div class="contact-input-labels">
                         <div class="alamat-email1">Alamat Email</div>
@@ -111,6 +114,7 @@
                         <option value="Male">Male</option>
                     </select>
                 </div>
+
                 <div class="save-button-container1">
                     <button class="button30" type="submit">
                         <div class="simpan1">Simpan</div>
@@ -118,32 +122,99 @@
                 </div>
             </div>
         </form>
+    </div>
+{{-- kata sandi --}}
+
+    <div id="rendersandi">
+        <div class="frame-wrapper59">
+            <div class="picture-container-parent">
+                <div class="picture-container">
+                    <div class="picture-inner-container">
+                        <div class="picture-holder">
+                            <img class="id-card-24dp-fill0-wght400-gra-icon3" alt="" src="{{url('EdulaExport/public/password-24dp-fill0-wght400-grad0-opsz24-1.svg')}}" />
+                        </div>
+                        <b class="data-pribadi3">Ubah Kata Sandi</b>
+                    </div>
+                </div>
+                <div class="frame-child102" style="width: 70%"></div>
+            </div>
+        </div>
+        <br>
+
+    <form   form action="" id="katasandiya">
+        <div class="">
+            <div class="katasandi">
+                <div class="nama-lengkap1">Kata Sandi Lama</div>
+                <div class="fill-namadepan1" style="width: 60%;">
+                    <div class="fill-namadepan-item"></div>
+                    <input class="nama-depan1" placeholder="Kata sandi lama" type="password" name="katasandilama" />
+                </div>
+                <br>
+                <div class="nama-lengkap1">Kata Sandi Baru</div>
+                <div class="fill-namabelakang1" style="width: 60%;">
+                    <div class="fill-namabelakang-item"></div>
+                    <input class="nama-belakang1" placeholder="Kata sandi baru" type="password" name="katasandibaru"/>
+                </div>
+
+                <div class="save-button-container1" style="margin: 100px 400px 0 0">
+                    <button class="button30" type="submit">
+                        <div class="simpan1">Simpan</div>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </form>
+    </div>
+
+    <div id="renderabout">
+        <div class="frame-wrapper59">
+            <div class="picture-container-parent">
+                <div class="picture-container">
+                    <div class="picture-inner-container">
+                        <div class="picture-holder">
+                            <img class="id-card-24dp-fill0-wght400-gra-icon3" alt="" src="{{url('EdulaExport/public/policy-24dp-fill0-wght400-grad0-opsz24-1.svg')}}" />
+                        </div>
+                        <b class="data-pribadi3">Kebijakan</b>
+                    </div>
+                </div>
+                <div class="frame-child102" style="width: 70%"></div>
+            </div>
+        </div>
+        <div class="textkebijakan" style="width: 300px, text-align: justify">
+            <p style="font-size: 14px">
+                <br>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, molestiae minima. <br>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, molestiae minima. <br>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, molestiae minima. <br>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, molestiae minima. <br>
+            </p>
+        </div>
+
+    </div>
     </main>
 </div>
 @endsection
 
 @section('JS')
 <script>
-    document.getElementById('dropdownButton').addEventListener('click', function() {
-        var content = document.getElementById('dropdownContent');
-        content.style.display = content.style.display === 'block' ? 'none' : 'block';
-    });
+    var divsandi=document.getElementById("rendersandi").innerHTML
+    var profile=document.getElementById("renderpick").innerHTML
+    var about=document.getElementById("renderabout").innerHTML
+    document.getElementById("renderabout").innerHTML=""
+    document.getElementById("rendersandi").innerHTML=''
 
-    function selectOption(value) {
-        document.getElementById('dropdownButton').textContent = value;
-        document.getElementById('dropdownContent').style.display = 'none';
-    }
-
-    window.onclick = function(event) {
-        if (!event.target.matches('.dropdown-button')) {
-            var dropdowns = document.getElementsByClassName('dropdown-content');
-            for (var i = 0; i < dropdowns.length; i++) {
-                var openDropdown = dropdowns[i];
-                if (openDropdown.style.display === 'block') {
-                    openDropdown.style.display = 'none';
-                }
-            }
+    function render(paramrender) {
+        document.getElementById("renderpick").innerHTML=""
+        if(paramrender==1){
+            document.getElementById("renderpick").innerHTML=profile
+        }else if(paramrender==2){
+            document.getElementById("renderpick").innerHTML=divsandi
+        }else{
+            document.getElementById("renderpick").innerHTML=about
         }
     }
+
+
+
 </script>
 @endsection

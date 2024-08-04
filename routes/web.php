3e7921bdbf88a9c6278ecfbb20df8929affb8893
lgarin211 <lagustinus211@gmail.com>
2024-08-04 17:37:34 +0700

    Co-authored-by: Yohanna Y <yohannayoe@users.noreply.github.com> Co-authored-by: MIRZHA CHAERANI <mirzha.chaerani@binus.ac.id> Co-authored-by: Ipenkk <Ipenkk@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -20,50 +20,35 @@ Route::get('/login', [AuthController::class, 'login']);
 Route::post('/login', [AuthController::class, 'ValidateLogin'])->name('login');
 Route::post('/register', [AuthController::class, 'ValidateRegister'])->name('register');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-// dasboard perlu di benarkan
-// Route::get('/dashboard', function () {
-//     return view('welcome');
-// });
-
 Route::any('/ReqPelajaran', [PelajarState::class, 'BuatPenajuan'])->name('BuatPenajuan');
 Route::any('/dashboard', [PelajarState::class, 'dashboard'])->name('dashboard');
 Route::get('/Rom', [PelajarState::class, 'Edetail'])->name('detail');
 Route::get('/Rom/setpo', [PelajarState::class, 'setpo'])->name('setpo');
 Route::get('/Rom/approve', [PelajarState::class, 'approve'])->name('approve');
-
 Route::get('/AmbilKelas', [PelajarState::class, 'AmbilKelas'])->name('AmbilKelas');
-
 Route::get('/UbahProfilPelajar', [UbahProfilController::class, 'ViewUbahProfil']);
 Route::get('/UbahProfilPengajar', [UbahProfilController::class, 'ViewUbahProfil']);
 Route::post('/UbahProfilPengajar/update', [UbahProfilController::class, 'UpdateUbahProfil'])->name('updateprofil');
-
 Route::get('/UbahPasswordPengajar', [UbahProfilController::class, 'ViewUbahPassword']);
 Route::post('/UbahPasswordPengajar/update', [UbahProfilController::class, 'UpdateUbahPassword'])->name('updatepassword');
-
-
-// Route::get('/riwayatpengajar', [PengajarController::class, 'riwayatpengajar'])->name('riwayatpengajar');
-
 Route::any('/JadiPengajar', [PengajarController::class, 'JadiPengajar'])->name('JadiPengajar');
-
-
 Route::post('/form-review', [PelajarController::class, 'store'])->name('form-review');
-
 Route::get('/DetailPengajar', [GuruController::class, 'show'])->name('DetailPengajar.show');
 Route::post('/DetailPengajar/{guru_id}/upload', [GuruController::class, 'store'])->name('DetailPengajar.store');
 Route::get('/payment/qris', [ipaymuController::class, 'initiateQrisPayment'])->name('payment.qris');
 Route::get('/payment/qris/callback', [ipaymuController::class, 'qrisPaymentCallback'])->name('payment.qris.callback');
-
-
-// Route::get('/riwayatpengajar/{id}', [PengajarController::class, 'riwayatpengajar'])->name('riwayatpengajar.show');
-
-// Route::get('/DetailPengajar', [GuruController::class, 'showall'])->name('DetailPengajar.showall');
-// ROUTER BENAR
 Route::get('/ReqKelasPayment', function () {
     return view('Pelajar.RequestKelasPayment');
 });
-
 Route::get('/FormTerima', [PengajarController::class, 'FormLink'])->name('FormTerima');
+Route::get('/CaraKerjaPelajar', function () {
+    return view('caraKerjaPelajar');
+});
+Route::get('/CaraKerjaPengajar', function () {
+    return view('caraKerjaPengajar');
+});
+Route::get('/search', [SearchController::class, 'search'])->name('search');
+Route::post('submit-meeting', [LinkController::class, 'submitMeeting'])->name('submitMeeting');
 
 // Route::get('/ReqKelasConfirm', function () {
 //     return view('Pelajar.RequestKelasConfirm');
@@ -72,19 +57,19 @@ Route::get('/FormTerima', [PengajarController::class, 'FormLink'])->name('FormTe
 // Route::get('/ReqKelasSchedule', function () {
 //     return view('Pelajar.RequestKelasSchedule');
 // });
+// Route::get('/riwayatpengajar/{id}', [PengajarController::class, 'riwayatpengajar'])->name('riwayatpengajar.show');
+
+// Route::get('/DetailPengajar', [GuruController::class, 'showall'])->name('DetailPengajar.showall');
+// ROUTER BENAR
 
 
+// Route::get('/riwayatpengajar', [PengajarController::class, 'riwayatpengajar'])->name('riwayatpengajar');
 
 
-
-
-Route::get('/CaraKerjaPelajar', function () {
-    return view('caraKerjaPelajar');
-});
-
-Route::get('/CaraKerjaPengajar', function () {
-    return view('caraKerjaPengajar');
-});
+// dasboard perlu di benarkan
+// Route::get('/dashboard', function () {
+//     return view('welcome');
+// });
 
 // Route::get('/detail1', function () {
 //     return view('detail1');
@@ -186,7 +171,6 @@ Route::get('/CaraKerjaPengajar', function () {
 // yohhhhh
 // UbahProfilPengajar
 
-Route::get('/search', [SearchController::class, 'search'])->name('search');
+
 // Route::post('/submit-meeting', [LinkController::class, 'submitMeeting'])->name('submitMeeting');
 // Route::get('submit-meeting', [LinkController::class, 'submitMeeting'])->name('submitMeeting');
-Route::post('submit-meeting', [LinkController::class, 'submitMeeting'])->name('submitMeeting');
